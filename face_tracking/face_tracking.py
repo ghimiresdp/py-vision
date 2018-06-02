d94fcd05b84effe2c3373a4754434f93dfb93d93
Sudip Ghimire <ghimiresdp@gmail.com>
2018-06-02 23:36:48 +0545

    small modification on classifier xml file path

--- face_tracking/face_tracking.py
+++ face_tracking/face_tracking.py
@@ -4,7 +4,9 @@ Originally created by Sudip Ghimire [ https://github.com/ghimiresdp ]
 """
 
 import cv2
+# if the code does not run, please change the file name with the full file path
 face = cv2.CascadeClassifier('face.xml')
+# face = cv2.CascadeClassifier('C:/face.xml')
 font = cv2.FONT_HERSHEY_SIMPLEX
 
 
@@ -23,19 +25,24 @@ def face_track():
             yy = y + h / 2
             ww, hh = pic.shape[:2]
             if xx < 0.5 * ww and xx != 0:
-                cv2.putText(pic, 'towards left', (x, y + h // 3), font, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
+                cv2.putText(pic, 'towards left', (x, y + h // 3),
+                            font, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
             elif xx > 0.8 * ww and xx != 0:
-                cv2.putText(pic, 'towards right', (x, y + h // 3), font, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
+                cv2.putText(pic, 'towards right', (x, y + h // 3),
+                            font, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
 
             if yy < 0.3 * hh and yy != 0:
-                cv2.putText(pic, 'towards top', (x, y + h * 2 // 3), font, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
+                cv2.putText(pic, 'towards top', (x, y + h * 2 // 3),
+                            font, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
             elif yy > 0.5 * hh and yy != 0:
-                cv2.putText(pic, 'towards bottom', (x, y + h * 2 // 3), font, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
+                cv2.putText(pic, 'towards bottom', (x, y + h * 2 // 3),
+                            font, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
             text = 2500.0 / w * 3.66
             text = int(text)
             txt = str(text) + "cm Far"
             # cv2.putText(pic, 'Target @ Centre',(x, y+h/2), font, 0.8, (255, 0, 0), 1, cv2.LINE_AA)
-            cv2.putText(pic, txt, (x+10, y + h // 2), font, 0.8, (0, 255, 255), 2, cv2.LINE_AA)
+            cv2.putText(pic, txt, (x+10, y + h // 2), font,
+                        0.8, (0, 255, 255), 2, cv2.LINE_AA)
 
         cv2.imshow("output", pic)
         if cv2.waitKey(1) & 0xFF == ord('q'):
